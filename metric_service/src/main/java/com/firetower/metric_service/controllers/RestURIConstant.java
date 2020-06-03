574e9f9f2475d4ba5d9df9879bce0ebf53a3af57
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2020-06-03 21:38:44 +0200

    test commit

--- metric_service/src/main/java/com/firetower/metric_service/controllers/RestURIConstant.java
+++ metric_service/src/main/java/com/firetower/metric_service/controllers/RestURIConstant.java
@@ -10,5 +10,6 @@ public class RestURIConstant {
     public static final String newMetrics = "/newMetrics";
     public static final String recent = "/recent";
     public static final String recents = "/recents";
+    
 
 }
