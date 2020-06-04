ac05504843df15ecd5a14944cb7d4c8a14ccb43f
victorvanleeuwen <v.vn.leeuwen@gmail.com>
2020-06-04 17:58:31 +0200

    update actions

--- user_service/src/main/java/com/firetower/user_service/common/models/User.java
+++ user_service/src/main/java/com/firetower/user_service/common/models/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 
